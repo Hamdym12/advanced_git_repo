91a0647aedbc7b7c74ab6060818b43dfe683c048
Hamdy Mohamed <hamdym12552@yahoo.com>
2025-04-29 08:12:25 +0300

    add text_button file

---
+++ lib/text_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OutlinedButton extends StatelessWidget {
+  const OutlinedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (){},
+      child: const Text('Text Button'),
+    );
+  }
+}
