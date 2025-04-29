f5237112804858162f112f0c4d27eca31e4485a8
Hamdy Mohamed <hamdym12552@yahoo.com>
2025-04-29 08:46:19 +0300

    added splash_screen

---
+++ lib/spalsh_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SpalshScreen extends StatelessWidget {
+  const SpalshScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Splash Screen')),
+    );
+  }
+}
