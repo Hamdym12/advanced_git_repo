b7314d4143568fe6cd06290d5936673cdac6ff56
Hamdy Mohamed <hamdym12552@yahoo.com>
2025-04-30 06:47:18 +0300

    add add_to_main file

---
+++ lib/add_to_main_4.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddToMain4 extends StatelessWidget {
+  const AddToMain4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
