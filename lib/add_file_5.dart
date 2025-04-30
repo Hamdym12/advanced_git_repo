b715353ee998294a64428b39764c1359859fb3fc
Hamdy Mohamed <hamdym12552@yahoo.com>
2025-04-30 06:50:02 +0300

    add add_file_5 file

---
+++ lib/add_file_5.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class AddFile5 extends StatelessWidget {
+  const AddFile5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
